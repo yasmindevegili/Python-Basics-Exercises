5315bc1c72a8854caedc74d6a423261e8f3afdb1
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2022-09-12 20:03:18 -0300

    Update 10.py

--- 10.py
+++ 10.py
@@ -2,4 +2,4 @@ nome = str(input())
 salario = float(input())
 vendas = float(input())
 salario_final = (0.15*vendas)+salario
-print('TOTAL = R$', salario_final, '\n')
+print('TOTAL = R$ {:.2f}'.format(salario_final))
