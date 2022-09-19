792901dea8ae9d5ca8b68b4be4cd57e371eb84bd
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2022-09-19 17:29:05 -0300

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,6 +1,7 @@
-a = int(input())
-b = int(input())
-c = int(input())
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
 MaiorAB = (a+b+abs(a-b))/2
 MaiorAB = (c+MaiorAB+abs(c-MaiorAB))/2
-print (MaiorAB, "eh o maior")
+print (int(MaiorAB), "eh o maior")
