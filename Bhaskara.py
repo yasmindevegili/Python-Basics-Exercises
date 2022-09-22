96c48317e70b30b74964325aaed408dfb5313df0
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2022-09-22 13:15:40 -0300

    Create Bhaskara.py

---
+++ Bhaskara.py
@@ -0,0 +1,14 @@
+import math
+A, B, C = input().split()
+A = float(A)
+B = float(B)
+C = float(C)
+delta = (B**2)-4*A*C
+if(A==0) or (delta<0):
+    print ("Impossivel calcular")
+    
+else:
+    root1 = (-B+math.sqrt(delta))/(2*A)
+    root2 = (-B-math.sqrt(delta))/(2*A)
+    print ("R1 = {:.5f}".format(root1),"\nR2 = {:.5f}".format(root2))
+    
