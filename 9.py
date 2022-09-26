defa9f36a4738d96faebbecd6866d7ddabb171e2
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2022-09-26 19:37:30 -0300

    Delete 9.py

--- 9.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-horas_trabalhadas = int(input())
-salario = float(input())
-salario_final = (horas_trabalhadas*salario)
-print('NUMBER =', n ) 
-print ('SALARY = U$ {:.2f}'.format(salario_final))
