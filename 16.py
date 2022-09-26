9381963494f779f3b36e35d40a957e2cf9ea37eb
Yasmin Devegili <48127784+yasmindevegili@users.noreply.github.com>
2022-09-26 19:35:48 -0300

    Delete 16.py

--- 16.py
+++
@@ -1,9 +0,0 @@
-import math
-x1, y1 = input().split()
-x2, y2 = input().split()
-x1 = float(x1)
-y1 = float(y1)
-x2 = float(x2)
-y2 = float(y2)
-distance = math.sqrt(pow(x2-x1,2)+pow(y2-y1,2))
-print ("{:.4f}".format(distance))
